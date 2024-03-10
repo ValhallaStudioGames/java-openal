68b5748b45afbb3002c13ddb3f1b1835081897be
BauwenDR <BauwenDR@gmail.com>
2024-03-10 20:47:36 +0100

    Added openal.dll for windows devices

--- lib/src/main/java/org/valhalla/openal/factory/ALFactory.java
+++ lib/src/main/java/org/valhalla/openal/factory/ALFactory.java
@@ -9,30 +9,30 @@ import org.valhalla.openal.jna.ALC;
 import org.valhalla.openal.jna.ALExt;
 
 public class ALFactory {
-	private static final String DEFAULT_DLL_NAME = "openal";
+    private static final String DEFAULT_DLL_NAME = "openal";
 
-	public final AL al;
-	public final ALC alc;
-	public final ALExt alext;
+    public final AL al;
+    public final ALC alc;
+    public final ALExt alext;
 
-	public ALFactory() {
-		al = Native.load(DEFAULT_DLL_NAME, AL.class);
-		alc = Native.load(DEFAULT_DLL_NAME, ALC.class);
-		alext = Native.load(DEFAULT_DLL_NAME, ALExt.class);
-	}
+    public ALFactory() {
+        al = Native.load(DEFAULT_DLL_NAME, AL.class);
+        alc = Native.load(DEFAULT_DLL_NAME, ALC.class);
+        alext = Native.load(DEFAULT_DLL_NAME, ALExt.class);
+    }
 
-	public ALFactory(File dllPath) throws FileNotFoundException {
-		String dllName = DEFAULT_DLL_NAME;
-		if (dllPath != null) {
-			if (!dllPath.exists()) {
-				throw new FileNotFoundException(dllPath.getAbsolutePath());
-			}
-			System.setProperty("jna.library.path", dllPath.getParent());
-			dllName = dllPath.getName();
-		}
+    public ALFactory(File dllPath) throws FileNotFoundException {
+        String dllName = DEFAULT_DLL_NAME;
+        if (dllPath != null) {
+            if (!dllPath.exists()) {
+                throw new FileNotFoundException(dllPath.getAbsolutePath());
+            }
+            System.setProperty("jna.library.path", dllPath.getParent());
+            dllName = dllPath.getName();
+        }
 
-		al = Native.load(dllName, AL.class);
-		alc = Native.load(dllName, ALC.class);
-		alext = Native.load(dllName, ALExt.class);
-	}
+        al = Native.load(dllName, AL.class);
+        alc = Native.load(dllName, ALC.class);
+        alext = Native.load(dllName, ALExt.class);
+    }
 }
